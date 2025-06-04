91227db15f1b615e31e6ce798bf1e3f6eb2b2795
小形克宏 <ogwata@vivliostyle.org>
2025-06-04 23:14:57 +0900

    vf2023reportを修正

--- ja/reports/vivliostyle-report-2023/conv.sh
+++ ja/reports/vivliostyle-report-2023/conv.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 pandoc -s --toc -c report.css -t html5 vf2023report.md -o vf2023report.html
-vivliostyle build --size A4 --output vf2023report-ja.pdf vf2023report.html 
+vivliostyle build --size A4 --output vf2023report-ja.pdf vf2023report.html
+pandoc -s --toc -c report.css -t html5 vf2023financial-report.md -o vf2023financial-report.html
+vivliostyle build --size A4 --output vf2023financial-report-ja.pdf vf2023financial-report.html 
