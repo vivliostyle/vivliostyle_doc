9cdd1c1eef75f9c01b22988008887d6874c449ac
小形克宏 <ogwata@vivliostyle.org>
2025-06-09 15:02:27 +0900

    古い財務報告スクリプトと関連ファイルを削除

--- conv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# HTMLファイルの生成
-vivliostyle build --size A4 --output vf2020financial-report.html vf2020financial-report.md
-
-# PDFファイルの生成
-vivliostyle build --size A4 --output vf2020financial-report.pdf vf2020financial-report.html 
