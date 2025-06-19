5d11cdc6a502ce09d403b72d1ae7fbe9fdc34d44
小形克宏 <ogwata@vivliostyle.org>
2025-06-19 18:07:58 +0900

    ja html pdf

--- ja/reports/vivliostyle-report-2024/conv.sh
+++ ja/reports/vivliostyle-report-2024/conv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pandoc -s --toc -c report.css -t html5 vf2023report.md -o vf2023report.html
-vivliostyle build --size A4 --output vf2023report-ja.pdf vf2023report.html
+vfm vf2024report.md > vf2024report.html
+vivliostyle build --size A4 --output vf2024report.pdf vf2024report.html
