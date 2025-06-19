9b1737839444ee560e8bee9214b363877afd91ee
小形克宏 <ogwata@vivliostyle.org>
2025-06-19 17:47:38 +0900

    typo

--- en/reports/vivliostyle-report-2023/conv.sh
+++ en/reports/vivliostyle-report-2023/conv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pandoc -s --toc -c report.css -t html5 vf2023report.md -o vf2023report.html
-vivliostyle build --size A4 --output vf2023report-en.pdf vf2023report.html 
+pandoc -s --toc -c report.css -t html5 vf2024report.md -o vf2024report.html
+vivliostyle build --size A4 --output vf2024report-en.pdf vf2024report.html 
